2281c2c5111139bea8d727a0f973a2b827199214
Konstantin Slepnev <slepnevkn@gmail.com>
2021-08-06 00:00:46 +0300

    task_final v2

--- task_final/solution.js
+++ task_final/solution.js
@@ -1,78 +1,63 @@
-// Исправьте функцию sendRequest
-// Аргументы функции:
-// - имя клиента
-// - телефон клиента
-// - объект с адресом доставки: {street, house, entrance, floor, flat}
-// - список товаров в заказе
-// - стоимость заказа с учетом скидок и доставки
-// Как результат функции требуется вернуть JSON,
-// cформированный в соответствии с правилами:
-// Объект data содержит все данные
-// В объекте data есть свойства:
-// - client - строка, имя клиента + через дефис телефон клиента;
-// - order - объект, содержащий данные о заказе:
-//     - address - адрес доставки
-//     - sum - стоимость заказа с учетом скидок и доставки
-// - goods: массив объектов с информацией о позициях заказа:
-//     - title - название позиции
-//     - count - количество в заказе
-// например:
-// {
-//    "data": {
-//      "client": "Иван +7(987)65-43-210",
-//      "order": {
-//        "address": "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53",
-//        "sum": 900
-//       },
-//       "goods": [
-//         {
-//           "title": "Пицца",
-//           "count": 2
-//         }
-//      ]
-//    }
-// }
-
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {
-      client: "",
-      order: {},
-      goods: [],
+  let data = {
+    client: "",
+    order: {},
+    goods: []
+  };
+
+  let countOfGoods = goods.length;
+
+  for (let i = 0; i < countOfGoods; i += 1) {
+    console.log(goods[i].title, goods[i].count);
+    let good = {
+      title: goods[i].title,
+      count: goods[i].count
     };
-
-    let countOfGoods = goods.length; 
-
-    for (let i = 0; i < countOfGoods; i += 1) {
-        data.goods.push(goods[i]);
-    }
-
-    data.order.address = address;
-    data.order.sum = sum;
-    data.client = name + " " + phone;
-
-     
-    let fullData = {data};
+    data.goods.push(good);
+  }
+  // ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53
+  let fullAddress = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд" + ", " + address.floor + " этаж" + ", кв " + address.flat;
   
-    let jsonData = JSON.stringify(fullData);
+  data.order.address = fullAddress;
+  console.log(fullAddress);
+  data.order.sum = sum;
+  data.client = name + " " + phone;
 
-    return jsonData;
+  let fullData = { data };
+
+  let jsonData = JSON.stringify(fullData);
+
+  return jsonData;
 }
-console.log (
-  sendRequest(
-    "Иван", // name
-   "+79001234567" , // phone
-    "пр. Невский, дом 101, 1 подъезд, 3 этаж, оф. 303", // address
-    [
-      {
-        "title": "Пицца",
-        "count": 2
-      },
-      {
-        "title": "Кофе",
-        "count": 3
-        
-      }
-    ], // goods
-     1000 // sum
-  )
-)
+
+// Входящие данные для отправки в функцию, которые изменять НЕЛЬЗЯ:
+let name = "Иван";
+let phone = "+7(987)65-43-210";
+let address = {
+  street: "Ленина",
+  house: "2",
+  entrance: "4",
+  floor: "5",
+  flat: "53"
+};
+let goods = [
+  {
+    id: 1,
+    title: "Пицца",
+    count: 2,
+    price: "500.00"
+  },
+  {
+    id: 2,
+    title: "Хачапури",
+    count: 6,
+    price: "800.00"
+  }
+];
+let sum = 900;
+
+// Вызов функции
+let result = sendRequest(name, phone, address, goods, sum);
+
+// Вывод результата в консоль
+console.log(result);
